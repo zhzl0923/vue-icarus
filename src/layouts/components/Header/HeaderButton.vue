ea28aa19edfb3e2ac18cec08b3462eefa75a6fdc
zzl1994 <zhanzili1994@163.com>
2021-10-12 21:53:16 +0800

    search

--- src/layouts/components/Header/HeaderButton.vue
+++ src/layouts/components/Header/HeaderButton.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="header-button">
-    <button>
+    <button @click="dialogVisible = true">
       <Icon icon="fa-solid:search" />
     </button>
+    <el-dialog
+      v-model="dialogVisible"
+      show-close="false"
+      width="40%"
+      :before-close="handleClose"
+    >
+      <template #title>
+        <el-input width="80%" prefix-icon="el-icon-search"></el-input>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { Icon } from "@iconify/vue";
 export default defineComponent({
   components: {
     Icon,
   },
-  setup() {},
+  setup() {
+    const dialogVisible = ref(false);
+    return { dialogVisible };
+  },
 });
 </script>
