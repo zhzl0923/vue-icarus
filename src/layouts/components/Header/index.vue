cee351364d2379a512ce09fde394bede06a45bd8
zzl1994 <zhanzili1994@163.com>
2021-10-19 22:04:10 +0800

    自适应

--- src/layouts/components/Header/index.vue
+++ src/layouts/components/Header/index.vue
@@ -2,9 +2,14 @@
   <div class="header">
     <avatar></avatar>
     <div class="container">
-      <menus></menus>
+      <div class="sm:block hidden">
+        <menus></menus>
+      </div>
       <div class="header-button">
         <search />
+        <div class="sm:hidden block">
+          <Icon icon="Bars"></Icon>
+        </div>
       </div>
     </div>
   </div>
@@ -15,12 +20,14 @@ import { defineComponent } from "vue";
 import Avatar from "./Avatar.vue";
 import Search from "./Search.vue";
 import Menus from "./Menus.vue";
+import { Icon } from "~/components/Icon";
 
 export default defineComponent({
   components: {
     Avatar,
     Search,
     Menus,
+    Icon,
   },
   setup() {
     return {};
