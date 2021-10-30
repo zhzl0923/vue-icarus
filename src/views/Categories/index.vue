2122808a7f1ea193eb14578a8d9709abbac2793e
zzl1994 <zhanzili1994@163.com>
2021-10-30 21:13:58 +0800

    category

--- src/views/Categories/index.vue
+++ src/views/Categories/index.vue
@@ -1,10 +1,63 @@
 <template>
-  <div>categories</div>
+  <n-card class="w-full">
+    <template #header>
+      <div class="flex space-x-2 text-center justify-center items-center text-xl">
+        <span>分类</span>
+        <span>-</span>
+        <span>13</span>
+      </div>
+    </template>
+    <template #default>
+      <n-tree
+          block-line
+          :data="data"
+          :default-expanded-keys="defaultExpandedKeys"
+          :selectable="false"
+      />
+    </template>
+  </n-card>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { h, defineComponent, ref } from 'vue'
+import {NTree,NCard,NButton, TreeOption} from "naive-ui";
 
+function createData (level = 4, baseKey = '') :TreeOption[] {
+  if (!level) return []
+  return Array.apply(null, { length: 6 - level } as unknown[]).map((_, index) => {
+    const key = '' + baseKey + level + index
+    const label = createLabel(level)
+    return {
+      label,
+      key,
+      children: createData(level - 1, key),
+      suffix: () =>
+          h(
+              NButton,
+              { text: true, type: 'primary' },
+              { default: () => 'Suffix' }
+          ),
+
+    }
+  })
+}
+
+function createLabel (level: number) {
+  if (level === 4) return '道生一'
+  if (level === 3) return '一生二'
+  if (level === 2) return '二生三'
+  if (level === 1) return '三生万物'
+}
 export default defineComponent({
-  setup() {},
+  components:{
+    NCard,
+    NButton,
+    NTree
+  },
+  setup() {
+    return {
+      data: createData(),
+      defaultExpandedKeys: ref(['40', '41'])
+    }
+  },
 });
 </script>
--- src/views/Tags/index.vue
+++ src/views/Tags/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>tags</div>
+  <n-card class="w-full">
+    <template #header>
+      <div class="flex space-x-2 text-center justify-center items-center text-xl">
+        <span>标签</span>
+        <span>-</span>
+        <span>13</span>
+      </div>
+    </template>
+    <template #default>
+
+    </template>
+  </n-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
+import {NButton, NCard,} from "naive-ui";
+
 
 export default defineComponent({
-  setup() {},
+  components: {
+    NCard,
+    NButton
+  },
+  setup() {
+  },
 });
 </script>
